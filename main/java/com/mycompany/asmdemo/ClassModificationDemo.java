194bd275450fbd59ffe25821cd0bed70e42df124
Deepak Arora <aroradeepak001@hotmail.com>
2016-06-01 00:07:24 +0530

    a

---
+++ main/java/com/mycompany/asmdemo/ClassModificationDemo.java
@@ -0,0 +1,25 @@
+package com.mycompany.asmdemo;
+
+public class ClassModificationDemo {
+
+private int version;
+    
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString(){
+        return "ClassCreationDemo: "+version;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new ClassModificationDemo());
+
+    }
+
+}
